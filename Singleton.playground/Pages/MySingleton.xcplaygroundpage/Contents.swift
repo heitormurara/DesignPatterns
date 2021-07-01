79bd72881a1b8e585fab7b46cffb7b74a092f600
Heitor Murara <heitormurara@gmail.com>
2021-07-01 07:30:04 -0300

    Udpate Singleton Playground

--- Singleton.playground/Pages/MySingleton.xcplaygroundpage/Contents.swift
+++ Singleton.playground/Pages/MySingleton.xcplaygroundpage/Contents.swift
@@ -1,6 +1,20 @@
-public class MySingleton {
-    static let shared = MySingleton()
-    private init() { }
+/// Use a Singleton when having more than one instance would cause problems
+class Singleton {
+    
+    static let shared = Singleton()
+    
+    private init() {}
 }
 
-let mySingleton = MySingleton.shared
+let singleton = Singleton.shared
+
+/// Use a Singleton Plus if a shared instance is useful _most_ of the time, but you also want custom instances
+class SingletonPlus {
+    
+    static let shared = SingletonPlus()
+    
+    init() {}
+}
+
+let singletonPlus1 = SingletonPlus.shared
+let singletonPlus2 = SingletonPlus()
